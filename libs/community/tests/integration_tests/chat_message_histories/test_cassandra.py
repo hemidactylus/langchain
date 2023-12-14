de72f094214145c12aa34604536d6b5478fd52d3
Stefano Lottini <stefano.lottini@datastax.com>
2023-12-14 15:13:43 -0800

    move/adapt tests for memory to community directory

--- libs/community/tests/integration_tests/chat_message_histories/test_cassandra.py
+++ libs/community/tests/integration_tests/chat_message_histories/test_cassandra.py
@@ -3,11 +3,11 @@ import time
 from typing import Optional
 
 from cassandra.cluster import Cluster
-from langchain.memory import ConversationBufferMemory
-from langchain.memory.chat_message_histories.cassandra import (
+from langchain_core.messages import AIMessage, HumanMessage
+
+from langchain_community.chat_message_histories.cassandra import (
     CassandraChatMessageHistory,
 )
-from langchain_core.messages import AIMessage, HumanMessage
 
 
 def _chat_message_history(
@@ -53,23 +53,17 @@ def _chat_message_history(
         )
 
 
-def test_memory_with_message_store() -> None:
+def test_message_history_base() -> None:
     """Test the memory with a message store."""
-    # setup cassandra as a message store
     message_history = _chat_message_history()
-    memory = ConversationBufferMemory(
-        memory_key="baz",
-        chat_memory=message_history,
-        return_messages=True,
-    )
 
-    assert memory.chat_memory.messages == []
+    assert message_history.messages == []
 
     # add some messages
-    memory.chat_memory.add_ai_message("This is me, the AI")
-    memory.chat_memory.add_user_message("This is me, the human")
+    message_history.add_ai_message("This is me, the AI")
+    message_history.add_user_message("This is me, the human")
 
-    messages = memory.chat_memory.messages
+    messages = message_history.messages
     expected = [
         AIMessage(content="This is me, the AI"),
         HumanMessage(content="This is me, the human"),
@@ -77,46 +71,31 @@ def test_memory_with_message_store() -> None:
     assert messages == expected
 
     # clear the store
-    memory.chat_memory.clear()
+    message_history.clear()
 
-    assert memory.chat_memory.messages == []
+    assert message_history.messages == []
 
 
-def test_memory_separate_session_ids() -> None:
+def test_message_history_separate_ids() -> None:
     """Test that separate session IDs do not share entries."""
     message_history1 = _chat_message_history(session_id="test-session1")
-    memory1 = ConversationBufferMemory(
-        memory_key="mk1",
-        chat_memory=message_history1,
-        return_messages=True,
-    )
     message_history2 = _chat_message_history(session_id="test-session2")
-    memory2 = ConversationBufferMemory(
-        memory_key="mk2",
-        chat_memory=message_history2,
-        return_messages=True,
-    )
 
-    memory1.chat_memory.add_ai_message("Just saying.")
+    message_history1.add_ai_message("Just saying.")
 
-    assert memory2.chat_memory.messages == []
+    assert message_history2.messages == []
 
-    memory1.chat_memory.clear()
-    memory2.chat_memory.clear()
+    message_history1.clear()
+    message_history2.clear()
 
 
-def test_memory_ttl() -> None:
+def test_message_history_with_ttl() -> None:
     """Test time-to-live feature of the memory."""
     message_history = _chat_message_history(ttl_seconds=5)
-    memory = ConversationBufferMemory(
-        memory_key="baz",
-        chat_memory=message_history,
-        return_messages=True,
-    )
-    #
-    assert memory.chat_memory.messages == []
-    memory.chat_memory.add_ai_message("Nothing special here.")
+
+    assert message_history.messages == []
+    message_history.add_ai_message("Nothing special here.")
     time.sleep(2)
-    assert memory.chat_memory.messages != []
+    assert message_history.messages != []
     time.sleep(5)
-    assert memory.chat_memory.messages == []
+    assert message_history.messages == []
